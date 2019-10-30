716d1ebc71452a052b209d13065f073dbad1200f
Shae <shae1811@gmail.com>
2019-10-30 12:07:03 +0800

    forecast updated

--- my-react-app/src/components/molecules/DailyWeather.js
+++ my-react-app/src/components/molecules/DailyWeather.js
@@ -1,17 +1,18 @@
-import React from 'react';
+  
+import React from 'react'
 import WeatherIcon from '../atoms/WeatherIcon';
 import HourlyWeather from './HourlyWeather';
 
-const DailyWeatherItem = ({ date, icon, list, maxTemp, minTemp, ...props }) => {
-  return (
-    <div>
-      <h3>{date}</h3>
-      <WeatherIcon icon={icon} />
-      <div>{maxTemp}</div>
-      <div>{minTemp}</div>
-      <HourlyWeather list={list} />
-    </div>
-  );
-};
+const DailyWeatherItem = ({date, icon, list, maxTemp, minTemp, ...props}) => {
+    return (
+        <div>
+            <h3>{date}</h3>
+            <WeatherIcon icon={icon}/>
+            <div>{maxTemp}</div>
+            <div>{minTemp}</div>
+            <HourlyWeather list={list}/>    
+        </div>
+    )
+}
 
-export default DailyWeatherItem;
+export default DailyWeatherItem
